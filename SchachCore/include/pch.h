8239486d55037746f9f6213e4fd4db9cbed6f137
Ivan-Krul <kiwiivanya128@gmail.com>
2024-02-27 21:26:35 +0100

    made requiring functions for Piece

--- SchachCore/include/pch.h
+++ SchachCore/include/pch.h
@@ -9,5 +9,6 @@
 
 // add headers that you want to pre-compile here
 #include "framework.h"
+#include "pieces.h"
 
 #endif //PCH_H
--- SchachCore/include/pieces.h
+++ SchachCore/include/pieces.h
@@ -1,17 +1,58 @@
 #pragma once
+//#pragma pack(push)
+//#pragma pack(1)
 
-typedef struct piece {
-    enum piecetype : char {
-        none,
-        king,
-        queen,
-        bishop,
-        knight,
-        rook,
-        pawn,
-        _count
-    } type : 3;
-    char color : 5;
+#define PIECE_COLOR_BITS 5
+#define PIECE_TYPE_BITS 3
+
+#define PIECE_COLOR_MASK ((1<<PIECE_COLOR_BITS)-1)
+#define PIECE_TYPE_MASK ((1<<PIECE_TYPE_BITS)-1)
+
+
+enum PieceType {
+    none,
+    king,
+    queen,
+    bishop,
+    knight,
+    rook,
+    pawn,
+    _count
 };
+typedef enum PieceType PieceType;
 
 
+struct Piece {
+    char color : PIECE_COLOR_BITS;
+    char type : PIECE_TYPE_BITS;
+};
+typedef struct Piece Piece;
+
+
+void set_piece_color(Piece* inst, const char color);
+void set_piece_type(Piece* inst, PieceType type);
+
+Piece set_piece(PieceType type, const char color) {
+    Piece p;
+    set_piece_color(&p, color);
+    set_piece_type(&p, type);
+    return p;
+}
+
+void set_piece_color(Piece* inst, const char color) {
+    inst->color = color & PIECE_COLOR_MASK;
+}
+
+char get_piece_color(const Piece inst) {
+    return inst.color << 6;
+}
+
+void set_piece_type(Piece* inst, PieceType type) {
+    inst->type = type & PIECE_TYPE_MASK;
+}
+
+PieceType get_piece_type(Piece inst) {
+    return (PieceType)inst.type;
+}
+
+//#pragma pack(pop)
