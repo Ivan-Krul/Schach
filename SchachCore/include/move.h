e6588af8417376b20d5a062ecf7ad44f3f2f6a52
Ivan-Krul <kiwiivanya128@gmail.com>
2024-03-10 19:35:02 +0100

    making move struct

--- SchachCore/include/move.h
+++ SchachCore/include/move.h
@@ -1,15 +1,15 @@
 #pragma once
 #include "board.h"
 
-struct coord {
+struct Coord {
     char x : BOARD_SIDE_CAPACITY;
     char y : BOARD_SIDE_CAPACITY;
 };
-typedef struct coord coord;
+typedef struct Coord Coord;
 
-struct move {
-    coord coord_from;
-    coord coord_to;
+struct Move {
+    Coord coord_from;
+    Coord coord_to;
     char en_passant_oppirtunity : 1;
     char castling : 1;
     char promotion : 3;
@@ -17,6 +17,14 @@ struct move {
     char mate : 1;
     char stalemate : 1;
 };
-typedef struct move move;
+typedef struct Move Move;
+
+Coord set_coord(char x, char y) {
+    Coord c;
+    c.x = x;
+    c.y = y;
+    return c;
+}
+
 
 
