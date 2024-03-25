f4582991670575afa37a712c0669c74372950755
Ivan-Krul <kiwiivanya128@gmail.com>
2024-03-25 22:52:02 +0100

    continue making vector struct

--- SchachCore/include/vector.h
+++ SchachCore/include/vector.h
@@ -1,9 +1,11 @@
 #pragma once
 #include <stdint.h>
 #include <malloc.h>
+#include <string.h>
 
 struct Vector {
     size_t size;
+    size_t capacity;
     void* collections;
 
     size_t colct_size;
@@ -15,6 +17,7 @@ Vector set_vector(size_t colct_size) {
     Vector vec;
     vec.colct_size = colct_size;
     vec.size = 0;
+    vec.capacity = 0;
     return vec;
 }
 
@@ -22,6 +25,7 @@ Vector set_memset_vector(size_t colct_size, size_t size, void* value) {
     Vector vec;
     vec.colct_size = colct_size;
     vec.size = size;
+    vec.capacity = size;
 
     size_t sizbyt = vec.size * vec.colct_size;
     vec.collections = malloc(sizbyt);
@@ -39,3 +43,17 @@ Vector set_memset_vector(size_t colct_size, size_t size, void* value) {
 
     return vec;
 }
+
+Vector set_memcpy_vector(size_t colct_size, size_t size, void* ptr) {
+    Vector vec;
+    vec.colct_size = colct_size;
+    vec.size = size;
+    vec.capacity = size;
+
+    memcpy(vec.collections, ptr, colct_size * size);
+
+    return vec;
+}
+
+
+
